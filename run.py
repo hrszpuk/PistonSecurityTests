5551d18ccfa371dc974645b5c2e54cdbbebb1dae
Remy <107559570+hrszpuk@users.noreply.github.com>
2023-10-19 02:54:51 +0100

    Added runtime checks

--- run.py
+++ run.py
@@ -11,13 +11,27 @@ files = list(filter(lambda file: file.startswith("test_") and file.endswith(".py
 print(f"{len(files)} test file{'s' if len(files) > 1 else ''} found.")
 
 # Checking connection to Piston API
-test_response = requests.get(PISTON_URL)
+try:
+    test_response = requests.get(PISTON_URL)
+except Exception as e:
+    print(f"Unable to establish a GET request to {PISTON_URL}. Exiting.")
+    exit(1)
 print(f"Piston status: {test_response.status_code} (ok?: {test_response.ok})")
 
 test_response = requests.get(PISTON_URL+"/api/v2/runtimes")
-print(test_response.json())
+if len(test_response.json()) < 1:
+    print("No runtimes found. Exiting.")
+    exit(1)
+
+print("Runtimes: ")
+for runtime in test_response.json():
+    print(f"- {runtime['language']} ({runtime['version']})")
 
 # Opening file contents and building request
+for filename in files:
+    with open(filename, 'r') as file:
+        contents = file.read()
+
 
 
 # Sending request to Piston API and displaying the result
