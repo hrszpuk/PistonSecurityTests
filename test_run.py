fd38379a2d4f2909ad47c3205bc2979f788456ba
hrs <107559570+hrszpuk@users.noreply.github.com>
2023-10-26 04:12:50 +0100

    run.py -> test_run.py
    Switch to using unittest

---
+++ test_run.py
@@ -0,0 +1,20 @@
+import unittest
+from utilities import *
+import requests
+
+
+class PistonSecurityTests(unittest.TestCase):
+
+    # Code should timeout after 3 seconds (unless maximum execution time is modified)
+    def test_code_execution_timeout(self):
+        setup_data_copy("test_code/timeout.py")
+        response = requests.post(PISTON_URL + "/api/v2/execute", json=data, headers=HEADERS)
+        json = response.json()
+        self.assertEqual("SIGKILL", json["run"]["signal"])
+
+    #
+    def test_outgoing_network_connections(self):
+        pass
+
+    def test_max_processes(self):
+        pass
