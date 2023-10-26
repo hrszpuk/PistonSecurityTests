5ef4b5db5900d6fb7f3379f84274d8f8e595054c
hrs <107559570+hrszpuk@users.noreply.github.com>
2023-10-26 04:54:36 +0100

    Updated PISTON_URL const variable usage

--- test_run.py
+++ test_run.py
@@ -27,13 +27,13 @@ class PistonSecurityTests(unittest.TestCase):
     # Code should timeout after 3 seconds (unless maximum execution time is modified)
     def test_code_execution_timeout(self):
         setup_data_copy("test_code/timeout.py")
-        response = requests.post(PISTON_URL + "/api/v2/execute", json=data, headers=HEADERS)
+        response = requests.post(PISTON_URL, json=data, headers=HEADERS)
         json = response.json()
         self.assertEqual("SIGKILL", json["run"]["signal"])
 
     # stdout should be capped at 65536
     def test_max_stdout_size(self):
         setup_data_copy("test_code/stdout.py")
-        response = requests.post(PISTON_URL + "/api/v2/execute", json=data, headers=HEADERS)
+        response = requests.post(PISTON_URL, json=data, headers=HEADERS)
         json = response.json()
         self.assertEqual("SIGKILL", json["run"]["signal"])
