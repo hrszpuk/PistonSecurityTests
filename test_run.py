4e1fc54d839133829103972f4958609019ab9225
hrs <107559570+hrszpuk@users.noreply.github.com>
2023-10-26 04:21:04 +0100

    Testing stdout.py cap

--- test_run.py
+++ test_run.py
@@ -5,6 +5,16 @@ import requests
 
 class PistonSecurityTests(unittest.TestCase):
 
+    #
+    def test_outgoing_network_connections(self):
+        pass
+
+    def test_max_processes(self):
+        pass
+
+    def test_max_files(self):
+        pass
+
     # Code should timeout after 3 seconds (unless maximum execution time is modified)
     def test_code_execution_timeout(self):
         setup_data_copy("test_code/timeout.py")
@@ -12,9 +22,9 @@ class PistonSecurityTests(unittest.TestCase):
         json = response.json()
         self.assertEqual("SIGKILL", json["run"]["signal"])
 
-    #
-    def test_outgoing_network_connections(self):
-        pass
-
-    def test_max_processes(self):
-        pass
+    # stdout should be capped at 65536
+    def test_max_stdout_size(self):
+        setup_data_copy("test_code/stdout.py")
+        response = requests.post(PISTON_URL + "/api/v2/execute", json=data, headers=HEADERS)
+        json = response.json()
+        self.assertEqual("SIGKILL", json["run"]["signal"])
